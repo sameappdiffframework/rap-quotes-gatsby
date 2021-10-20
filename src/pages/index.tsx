7b28ef72311fe4cb27d3f8a9a709115979e48989
Colby M. White <colbywhite@users.noreply.github.com>
2021-10-20 15:36:05 -0500

    feat: use image plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import React from "react";
 import Layout from '../components/layout';
 import '../components/quote.css';
@@ -12,7 +13,10 @@ const IndexPage = (): JSX.Element => {
                     circumstance.
                 </p>
             </blockquote>
-            <img src="https://i.scdn.co/image/ab67616d0000b27357641c5581a8d6527741748a"/>
+            <StaticImage src="https://i.scdn.co/image/ab67616d0000b27357641c5581a8d6527741748a"
+                         placeholder="blurred"
+                         className="album"
+                         alt="Black Rock"/>
             <figcaption>
                 <p>Joell Ortiz</p>
                 <cite><a href="https://open.spotify.com/album/5kCNU9ouaRoRtQNJrtfavl" target="_blank">Black Rock</a></cite>
@@ -24,7 +28,10 @@ const IndexPage = (): JSX.Element => {
                     The boy's slick like three kids stacked in an overcoat at a twenty-one and over show.
                 </p>
             </blockquote>
-            <img src="https://i.scdn.co/image/ab67616d0000b273b7ddccc528879dbacb7662bb"/>
+            <StaticImage src="https://i.scdn.co/image/ab67616d0000b273b7ddccc528879dbacb7662bb"
+                         placeholder="blurred"
+                         className="album"
+                         alt="Lice Team, Baby"/>
             <figcaption>
                 <p>Aesop Rock</p>
                 <cite><a href="https://open.spotify.com/album/3KJsSVL5vtJlb11tLHjdQN" target="_blank">Lice Team,
@@ -37,7 +44,10 @@ const IndexPage = (): JSX.Element => {
                     From the get-go, this pop can cause a panic at the disco.
                 </p>
             </blockquote>
-            <img src="https://i.scdn.co/image/ab67616d0000b2739b7da1018417380cf15e54b9"/>
+            <StaticImage src="https://i.scdn.co/image/ab67616d0000b2739b7da1018417380cf15e54b9"
+                         placeholder="blurred"
+                         className="album"
+                         alt="Hard Living"/>
             <figcaption>
                 <p>Method Man</p>
                 <cite><a href="https://open.spotify.com/album/7q3ff38RxgNN0HbG5WNDWF" target="_blank">Hard Living</a></cite>
@@ -49,7 +59,10 @@ const IndexPage = (): JSX.Element => {
                     You give me steps to climb, I redesign. I made some bleachers.
                 </p>
             </blockquote>
-            <img src="https://i.scdn.co/image/ab67616d0000b27357641c5581a8d6527741748a"/>
+            <StaticImage src="https://i.scdn.co/image/ab67616d0000b27357641c5581a8d6527741748a"
+                         placeholder="blurred"
+                         className="album"
+                         alt="No Trouble"/>
             <figcaption>
                 <p>Oddisee</p>
                 <cite><a href="https://open.spotify.com/album/5kCNU9ouaRoRtQNJrtfavl" target="_blank">No Trouble</a></cite>
