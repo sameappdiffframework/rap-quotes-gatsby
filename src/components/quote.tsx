51dc051858e4dba8fffa4f980a467901dab3e848
Colby M. White <colbywhite@users.noreply.github.com>
2021-10-20 16:18:05 -0500

    feat: pull quotes from json

---
+++ src/components/quote.tsx
@@ -0,0 +1,20 @@
+import '../components/quote.css';
+import React from 'react';
+import { QuoteModel } from './quote-model';
+
+export default function Quote({ quote }: { quote: QuoteModel }) {
+    return (
+        <figure>
+            <blockquote>
+                <p>{quote.quote}</p>
+            </blockquote>
+            <img src={quote.source.image} alt={quote.source.name}/>
+            <figcaption>
+                <p>{quote.artist.name}</p>
+                <cite>
+                    <a href={quote.source.url} target="_blank">{quote.source.name}</a>
+                </cite>
+            </figcaption>
+        </figure>
+    );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,14 @@
-import { StaticImage } from 'gatsby-plugin-image'
 import React from "react";
 import Layout from '../components/layout';
-import '../components/quote.css';
+import Quote from '../components/quote';
+import { QuoteModel } from "../components/quote-model";
+import JSONData from "../components/quotes.json"
 
+const quotes: QuoteModel[] = JSONData;
 const IndexPage = (): JSX.Element => {
   return (
     <Layout pageTitle={'Rap Quotes'}>
-        <figure>
-            <blockquote>
-                <p>
-                    Y'all wearing dresses and purses, dressing in Birkins, fam. I put a cap in that gown - pop and
-                    circumstance.
-                </p>
-            </blockquote>
-            <StaticImage src="https://i.scdn.co/image/ab67616d0000b27357641c5581a8d6527741748a"
-                         placeholder="blurred"
-                         className="album"
-                         alt="Black Rock"/>
-            <figcaption>
-                <p>Joell Ortiz</p>
-                <cite><a href="https://open.spotify.com/album/5kCNU9ouaRoRtQNJrtfavl" target="_blank">Black Rock</a></cite>
-            </figcaption>
-        </figure>
-        <figure>
-            <blockquote>
-                <p>
-                    The boy's slick like three kids stacked in an overcoat at a twenty-one and over show.
-                </p>
-            </blockquote>
-            <StaticImage src="https://i.scdn.co/image/ab67616d0000b273b7ddccc528879dbacb7662bb"
-                         placeholder="blurred"
-                         className="album"
-                         alt="Lice Team, Baby"/>
-            <figcaption>
-                <p>Aesop Rock</p>
-                <cite><a href="https://open.spotify.com/album/3KJsSVL5vtJlb11tLHjdQN" target="_blank">Lice Team,
-                    Baby</a></cite>
-            </figcaption>
-        </figure>
-        <figure>
-            <blockquote>
-                <p>
-                    From the get-go, this pop can cause a panic at the disco.
-                </p>
-            </blockquote>
-            <StaticImage src="https://i.scdn.co/image/ab67616d0000b2739b7da1018417380cf15e54b9"
-                         placeholder="blurred"
-                         className="album"
-                         alt="Hard Living"/>
-            <figcaption>
-                <p>Method Man</p>
-                <cite><a href="https://open.spotify.com/album/7q3ff38RxgNN0HbG5WNDWF" target="_blank">Hard Living</a></cite>
-            </figcaption>
-        </figure>
-        <figure>
-            <blockquote>
-                <p>
-                    You give me steps to climb, I redesign. I made some bleachers.
-                </p>
-            </blockquote>
-            <StaticImage src="https://i.scdn.co/image/ab67616d0000b27357641c5581a8d6527741748a"
-                         placeholder="blurred"
-                         className="album"
-                         alt="No Trouble"/>
-            <figcaption>
-                <p>Oddisee</p>
-                <cite><a href="https://open.spotify.com/album/5kCNU9ouaRoRtQNJrtfavl" target="_blank">No Trouble</a></cite>
-            </figcaption>
-        </figure>
+        {quotes.map(quote => (<Quote quote={quote} key={quote.id}/>))}
     </Layout>
   )
 }
